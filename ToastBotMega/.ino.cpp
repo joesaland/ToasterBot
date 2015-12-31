c5c178fd5b1cc928b08ed692695cf001e0ab37f6
gidaeon <joe@saland.com>
2015-12-31 11:17:33 -0700

    Motor Test Fuctions

--- ToastBotMega/.ino.cpp
+++ ToastBotMega/.ino.cpp
@@ -1,10 +1,14 @@
 //This is a automatic generated file
 //Please do not modify this file
 //If you touch this file your change will be overwritten during the next build
-//This file has been generated on 2015-12-31 09:44:39
+//This file has been generated on 2015-12-31 11:16:14
 
 #include "Arduino.h"
+#include "Servo.h"
+#include "NewPing.h"
+#include "AFMotor.h"
 void setup() ;
+void motorTest() ;
 void loop() ;
 
 #include "ToastBotMega.ino"
--- ToastBotMega/ToastBotMega.ino
+++ ToastBotMega/ToastBotMega.ino
@@ -3,13 +3,64 @@
 #include "NewPing.h"
 #include "AFMotor.h"
 
-void setup()
-{
+AF_DCMotor motorFR(1); // define motor on channel 1 with 1KHz default PWM
+AF_DCMotor motorFL(2); // define motor on channel 2 with 1KHz default PWM
+AF_DCMotor motorBR(4); // define motor on channel 4 with 1KHz default PWM
+AF_DCMotor motorBL(3); // define motor on channel 3 with 1KHz default PWM
+
+
+void setup() {
 // Add your initialization code here
+
+	AF_DCMotor motorFR(1); // define motor on channel 1 with 1KHz default PWM
+	AF_DCMotor motorFL(2); // define motor on channel 2 with 1KHz default PWM
+	AF_DCMotor motorBR(4); // define motor on channel 4 with 1KHz default PWM
+	AF_DCMotor motorBL(3); // define motor on channel 3 with 1KHz default PWM
+}
+
+void motorTest() {
+
+	motorFR.run(FORWARD);      // turn it on going forward
+	motorFL.run(FORWARD);      // turn it on going forward
+	motorBR.run(FORWARD);      // turn it on going forward
+	motorBL.run(FORWARD);      // turn it on going forward
+	delay(1000);
+
+	motorFR.run(BACKWARD);      // turn it on going forward
+	motorFL.run(FORWARD);      // turn it on going forward
+	motorBR.run(BACKWARD);      // turn it on going forward
+	motorBL.run(FORWARD);      // turn it on going forward
+	delay(1000);
+
+	motorFR.run(FORWARD);      // turn it on going forward
+	motorFL.run(FORWARD);      // turn it on going forward
+	motorBR.run(FORWARD);      // turn it on going forward
+	motorBL.run(FORWARD);      // turn it on going forward
+	delay(1000);
+
+	motorFR.run(FORWARD);      // turn it on going forward
+	motorFL.run(BACKWARD);      // turn it on going forward
+	motorBR.run(FORWARD);      // turn it on going forward
+	motorBL.run(BACKWARD);      // turn it on going forward
+	delay(1000);
+
+	motorFR.run(BACKWARD);      // turn it on going forward
+	motorFL.run(BACKWARD);      // turn it on going forward
+	motorBR.run(BACKWARD);      // turn it on going forward
+	motorBL.run(BACKWARD);      // turn it on going forward
+	delay(1000);
+
+	motorFR.run(RELEASE);      // stopped
+	motorFL.run(RELEASE);      // stopped
+	motorBR.run(RELEASE);      // stopped
+	motorBL.run(RELEASE);      // stopped
+	delay(1000);
 }
 
 // The loop function is called in an endless loop
-void loop()
-{
+void loop() {
 //Add your repeated code here
+
+	motorTest();
+
 }
