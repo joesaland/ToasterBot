3e68b98b4cac3d295fa9dcbe454dda5dd58d859e
gidaeon <joe@saland.com>
2015-12-31 10:04:14 -0700

    Stuff!!

--- ToastBotMega/ToastBotMega.ino
+++ ToastBotMega/ToastBotMega.ino
@@ -1,7 +1,13 @@
 //The setup function is called once at startup of the sketch
+
+int Stuff;
+
 void setup()
 {
 // Add your initialization code here
+
+	Stuff = 0;
+
 }
 
 // The loop function is called in an endless loop
